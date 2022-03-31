ce13660dd2ca60690e6d2986d8462176881ff669
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2022-03-31 10:04:10 +0700

    fixing: pagination

--- app/Traits/QueryBuilder.php
+++ app/Traits/QueryBuilder.php
@@ -8,18 +8,42 @@ trait QueryBuilder
 {
     use ApiResponser;
 
-    public function queryBuilder($subject, ...$column)
+    public function queryBuilder($subject, $column)
     {
         if (is_subclass_of($subject, Model::class)) {
             $subject = $subject::query();
         }
 
         if (request()->has('search') && request()->get('search') != '') {
-            $subject = $subject->where(function ($query) use ($column) {
+            // if $column is array
+            if (is_array($column)) {
                 foreach ($column as $col) {
-                    $query->orWhere($col, 'like', '%'.request()->get('search').'%');
+                // if $column has a dot, then it's a relation
+                    if (strpos($col, '.') !== false) {
+                        $subject->orWhereHas(
+                            explode('.', $col)[0],
+                            function ($query) use ($col, $subject) {
+                                $query->where(
+                                    explode('.', $col)[1],
+                                    'like',
+                                    '%' . request()->get('search') . '%'
+                                );
+                            }
+                        );
+                    } else {
+                        $subject->orWhere($col, 'like', '%' . request()->get('search') . '%');
+                    }
                 }
-            });
+            } else {
+                // if $column has a dot, then it's a relation
+                if (strpos($column, '.') !== false) {
+                    $subject = $subject->whereHas($column, function ($query) {
+                        $query->where('name', 'like', '%' . request()->get('search') . '%');
+                    });
+                } else {
+                    $subject = $subject->where($column, 'like', '%' . request()->get('search') . '%');
+                }
+            }
         }
 
         if (request()->has('except') && request()->get('except') > 0) {
@@ -35,11 +59,14 @@ trait QueryBuilder
         return $subject;
     }
 
-    public function queryPaginate($subject, $data = 'berikut', $resource = null)
+    public function queryPaginate($subject, String $nama = 'berikut', $resource = null)
     {
         // paginate handling
         if (request()->has('per_page') && request()->get('per_page') > 0) {
             $collection = $subject->paginate(request()->get('per_page'));
+            $collection->each(function ($collect, $index) use ($collection) {
+                $collect->nomor = $collection->perPage() * ($collection->currentPage() - 1) + $index + 1;
+            });
         } else {
             $collection = $subject->get();
         }
@@ -47,13 +74,13 @@ trait QueryBuilder
         if (count($collection) > 0 || (isset($collection->data) && count($collection->data) > 0)) {
             if ($resource == null) {
                 // collection result
-                $data = $this->successResponse($collection, $data.' berhasil ditampilkan', 200);
+                $data = $this->successResponse($collection, $nama.' berhasil ditampilkan', 200);
             } else {
                 // resource result
-                $data = $this->responseWithResourceCollection($resource::collection($collection), $data.' berhasil ditampilkan', 200);
+                $data = $this->responseWithResourceCollection($resource::collection($collection), $nama.' berhasil ditampilkan', 200);
             }
         } else {
-            $data = $this->notFoundResponse($data);
+            $data = $this->notFoundResponse($nama);
         }
 
         return $data;
