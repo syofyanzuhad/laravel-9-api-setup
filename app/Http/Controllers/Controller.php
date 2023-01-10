3ecf6f38dc940c8efa60a6fcae1dc921e2664ef2
syofyanzuhad <sofyanzuhad2@gmail.com>
2023-01-10 07:38:37 +0700

    add: queryBuilderTrait-controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Traits\ApiResponser;
+use App\Traits\QueryBuilder;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,5 +11,5 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponser;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponser, QueryBuilder;
 }
